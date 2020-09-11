674c8cc99db85ee8449e2ce390d80fb3fbd028e3
yuxiRen <rita.ren.yuxi@gmail.com>
2020-09-10 21:18:49 -0700

    Create MyBatis mapper class with USERS

--- src/main/java/com/rita/chatII/Mapper/UserMapper.java
+++ src/main/java/com/rita/chatII/Mapper/UserMapper.java
@@ -1,9 +1,17 @@
 package com.rita.chatII.Mapper;
 
+import com.rita.chatII.Model.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
-public class UserMapper {
+public interface UserMapper {
+    @Select("SELECT * FROM USERS WHERE username = #{name}")
+    User getUser(String name);
 
+    @Insert("INSERT INTO USERS (username, salt, password, firstname, lastname) VALUES(#{name}, #{salt}, #{password}, #{firstName}, #{lastName})")
+    @Options(useGeneratedKeys = true, keyProperty = "userId")
+    int insert(User user);
 }
