47e469b18daa18f1024219135bc15fba7749c717
yuxiRen <rita.ren.yuxi@gmail.com>
2020-09-10 21:01:39 -0700

    create User class to match the USER database

--- src/main/java/com/rita/chatII/Model/ChatMessage.java
+++ src/main/java/com/rita/chatII/Model/ChatMessage.java
@@ -3,6 +3,15 @@ package com.rita.chatII.Model;
 public class ChatMessage {
     private String name;
     private String msg;
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public ChatMessage(String name, String msg) {
         this.name = name;
