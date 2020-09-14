361669b7bd90d6e3177f9f3d3273e90103535eec
yuxiRen <rita.ren.yuxi@gmail.com>
2020-09-13 21:04:35 -0700

    signup template

--- src/main/java/com/rita/chatII/config/SecurityConfig.java
+++ src/main/java/com/rita/chatII/config/SecurityConfig.java
@@ -32,6 +32,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
 
         http.formLogin()
-                .defaultSuccessUrl("/home", true);
+                .defaultSuccessUrl("/chat", true);
     }
 }
